2b6ed236e08a9cc348740ef68d13b1b2a386d38c
Jani Pirisjoki <jpir@iki.fi>
2023-12-05 21:58:35 +0200

    Part3: Assignment 3.10 fix port

--- index.js
+++ index.js
@@ -148,7 +148,7 @@ const addPerson = (request, response) => {
 
 app.post(`${apiRoot}/persons`, addPerson);
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
     console.log(`Server running on ${PORT}`);
